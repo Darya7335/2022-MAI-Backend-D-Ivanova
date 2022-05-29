4a54163e79128b7ce238f07d89697287dbc19545
user <dashaivanova.1998@mail.ru>
2022-05-29 13:23:32 -0500

    Add Lab1

---
+++ LR1/main.py
@@ -0,0 +1,15 @@
+from cache import LRUCache
+
+cache = LRUCache(100)
+cache.set('Jesse', 'Pinkman')
+# print(cache.cache)
+cache.set('Walter', 'White')
+# print(cache.cache)
+cache.set('Jesse', 'James')
+# print(cache.cache)
+cache.get('Jesse') # вернёт 'James'
+# print(cache.cache)
+cache.rem('Walter')
+# print(cache.cache)
+cache.get('Walter') # вернёт ''
+# print(cache.cache)
